c797b969400ef47400125d24604b70cb9ac2f421
RawNatural <100200437+RawNatural@users.noreply.github.com>
2022-08-04 15:53:50 +1000

    Update firstToken.sol

--- firstToken.sol
+++ firstToken.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract firstToken is ERC20 {
+//Makes new ERC20 token
     constructor(uint256 initialSupply) ERC20("FirstToken", "FST") {
         _mint(msg.sender, initialSupply);
     }
