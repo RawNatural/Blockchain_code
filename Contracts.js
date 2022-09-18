aa1946f7fa11267e0982fdfb71f03bf5cfa30bd2
RawNatural <100200437+RawNatural@users.noreply.github.com>
2022-09-19 07:17:51 +1000

    Add files via upload

---
+++ Contracts.js
@@ -0,0 +1,77 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Deployment', () => {
+
+  beforeEach(async () => {
+    // Setup accounts
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+
+    // Load contracts
+    const DeployableSmartContract = await ethers.getContractFactory('DeployableSmartContract');
+    //const FlashLoanSimpleReceiverBase = await ethers.getContractFactory('AaveFlashLoanReceiver');
+    const FlashLoanReceiver = await ethers.getContractFactory('FlashLoanReceiver');
+
+
+
+    //Deploy DeployableSmartContract THIS WORKS DONT CHANGE
+    deployedSmartContract = await DeployableSmartContract.deploy();
+
+    //Deploy FlashLoanReceiver
+    flashLoanReceiver = await FlashLoanReceiver.deploy('0x794a61358D6845594F94dc1DB02A252b5b4814aD');
+
+
+    //Deploy aaveFlashLoanReceiver
+    //aaveFlashLoanReceiver = await FlashLoanSimpleReceiverBase.deploy('0x794a61358D6845594F94dc1DB02A252b5b4814aD');
+
+  }) // end beforeEach
+
+  //it deployed DeployableSmartContract 
+  it('deployed DeployableSmartContract', async () => {
+    expect(await deployedSmartContract.deployed)
+  })
+
+/*
+  it('deployed aaveFlashLoanReceiver', async () => {
+    expect(await aaveFlashLoanReceiver.deployed)
+  })
+
+  */
+
+  it('deployed FlashLoanReceiver', async () => {
+    expect(await flashLoanReceiver.deployed);
+  })
+
+
+
+})
+
+describe('Executing Flash Loan', () => {
+  it('Executes Flash Loan', async () => {
+    let flashAddress = await flashLoanReceiver.getFlashLoanAddress()
+    //console.log(flashAddress)
+    //expect(await flashLoanReceiver.printAddress().to.equal('0x794a61358D6845594F94dc1DB02A252b5b4814aD'))
+    console.log(deployer.address)
+    let owner = await flashLoanReceiver.getOwner()
+    console.log(owner)
+  })
+})
+/*
+describe('Borrowing funds', () => {
+
+    it('borrows funds from the pool', async () => {
+      let amount = tokens(100)
+      let transaction = await flashLoanReceiver.connect(deployer).executeFlashLoan(amount)
+      let result = await transaction.wait()
+
+      await expect(transaction).to.emit(flashLoanReceiver, 'LoanReceived')
+        .withArgs(token.address, amount)
+    })
+
+
+  })*/
