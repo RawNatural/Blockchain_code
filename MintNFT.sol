d4fac4121e99a0f6eca549fdcca8ae885fb629a4
RawNatural <100200437+RawNatural@users.noreply.github.com>
2022-09-20 14:45:16 +1000

    Create MintNFT.sol

---
+++ MintNFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: unlicensed
+
+pragma solidity 0.5.0;
+
+import "./ERC721Full.sol";
+
+contract MintNFT is ERC721Full {
+
+    constructor() ERC721Full("New NFT", "NFT") public {
+    }
+
+    function mint(address _to, string memory _tokenURI) public returns(bool) {
+       uint _tokenId = totalSupply().add(1);
+       _mint(_to, _tokenId);
+       _setTokenURI(_tokenId, _tokenURI);
+       return true;
+    }
+}
