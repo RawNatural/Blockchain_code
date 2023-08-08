532ead4b438221e722eac8161eb2ae9b48e04528
RawNatural <100200437+RawNatural@users.noreply.github.com>
2023-08-08 23:32:17 +1200

    CustomCoin.sol

--- Momint_App_Contracts/CustomCoin.sol
+++ Momint_App_Contracts/CustomCoin.sol
@@ -1,1 +1,10 @@
-//Hello
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+import "./CustomERC20.sol";
+
+contract CustomCoin is CustomERC20 {
+    constructor(uint256 initialSupply) public CustomERC20("TestMomintCustomToken", "MMC") {
+        mint(msg.sender, initialSupply);
+    }
+}
